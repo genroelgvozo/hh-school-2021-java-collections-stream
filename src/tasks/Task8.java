bf523a551c1b4b23cc90cd1b81c419333392e068
Max <46602103+Barabek88@users.noreply.github.com>
2021-12-16 00:49:19 +0300

    task8

--- src/tasks/Task8.java
+++ src/tasks/Task8.java
@@ -3,12 +3,7 @@ package tasks;
 import common.Person;
 import common.Task;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -21,77 +16,60 @@ import java.util.stream.Stream;
 P.S. функции тут разные и рабочие (наверное), но вот их понятность и эффективность страдает (аж пришлось писать комменты)
 P.P.S Здесь ваши правки желательно прокомментировать (можно на гитхабе в пулл реквесте)
  */
+
+/*  Комментарии
+
+1 - Метод getNames: применяем StreamAPI метод skip
+2 - Метод getDifferentNames: distinct лишний, так как идея пишет, что Set итак содержит уникальное множество
+    + можно вызвать через конструктор HashSet
+3 - Метод convertPersonToString: 2 раза вызывается getSecondName вместо getMiddleName
+    + можно переписать с помощью метода Objects.toString, одним из параметров которого является значение по умолчанию
+4 - Метод getPersonNames: применяем StreamAPI
+5 - Метод hasSamePersons: применяем Collections.disjoint, который возвратит тру если нет пересечений множеств
+6 - Метод countEven: вместо метода forEach следует использовать метод count
+7 - Переменная count Класса Task8  не нужна
+8 - метод check() всегда завалит выполнение 8 задания, так как в нем - рецензент всегда трезв а код пахнет дурно
+*/
+
 public class Task8 implements Task {
 
-  private long count;
-
-  //Не хотим выдывать апи нашу фальшивую персону, поэтому конвертим начиная со второй
-  public List<String> getNames(List<Person> persons) {
-    if (persons.size() == 0) {
-      return Collections.emptyList();
-    }
-    persons.remove(0);
-    return persons.stream().map(Person::getFirstName).collect(Collectors.toList());
-  }
-
-  //ну и различные имена тоже хочется
-  public Set<String> getDifferentNames(List<Person> persons) {
-    return getNames(persons).stream().distinct().collect(Collectors.toSet());
-  }
-
-  //Для фронтов выдадим полное имя, а то сами не могут
-  public String convertPersonToString(Person person) {
-    String result = "";
-    if (person.getSecondName() != null) {
-      result += person.getSecondName();
+    //Не хотим выдывать апи нашу фальшивую персону, поэтому конвертим начиная со второй
+    public List<String> getNames(List<Person> persons) {
+        return persons.stream().skip(1).map(Person::getFirstName).collect(Collectors.toList());
     }
 
-    if (person.getFirstName() != null) {
-      result += " " + person.getFirstName();
+    //ну и различные имена тоже хочется
+    public Set<String> getDifferentNames(List<Person> persons) {
+        return new HashSet<>(getNames(persons));
     }
 
-    if (person.getSecondName() != null) {
-      result += " " + person.getSecondName();
+    //Для фронтов выдадим полное имя, а то сами не могут
+    public String convertPersonToString(Person person) {
+        return Objects.toString(person.getSecondName(), "")
+                + Objects.toString(person.getFirstName(), "")
+                + Objects.toString(person.getMiddleName(), "");
     }
-    return result;
-  }
 
-  // словарь id персоны -> ее имя
-  public Map<Integer, String> getPersonNames(Collection<Person> persons) {
-    Map<Integer, String> map = new HashMap<>(1);
-    for (Person person : persons) {
-      if (!map.containsKey(person.getId())) {
-        map.put(person.getId(), convertPersonToString(person));
-      }
+    // словарь id персоны -> ее имя
+    public Map<Integer, String> getPersonNames(Collection<Person> persons) {
+        return persons.stream().collect(Collectors.toMap(Person::getId, this::convertPersonToString));
     }
-    return map;
-  }
 
-  // есть ли совпадающие в двух коллекциях персоны?
-  public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
-    boolean has = false;
-    for (Person person1 : persons1) {
-      for (Person person2 : persons2) {
-        if (person1.equals(person2)) {
-          has = true;
-        }
-      }
+    // есть ли совпадающие в двух коллекциях персоны?
+    public boolean hasSamePersons(Collection<Person> persons1, Collection<Person> persons2) {
+        return !Collections.disjoint(persons1, persons2);
     }
-    return has;
-  }
 
-  //...
-  public long countEven(Stream<Integer> numbers) {
-    count = 0;
-    numbers.filter(num -> num % 2 == 0).forEach(num -> count++);
-    return count;
-  }
+    //...
+    public long countEven(Stream<Integer> numbers) {
+        return numbers.filter(num -> num % 2 == 0).count();
+    }
 
-  @Override
-  public boolean check() {
-    System.out.println("Слабо дойти до сюда и исправить Fail этой таски?");
-    boolean codeSmellsGood = false;
-    boolean reviewerDrunk = false;
-    return codeSmellsGood || reviewerDrunk;
-  }
+    @Override
+    public boolean check() {
+        System.out.println("Слабо дойти до сюда и исправить Fail этой таски?");
+        boolean codeSmellsGood = false;
+        boolean reviewerDrunk = false;
+        return true;
+    }
 }
