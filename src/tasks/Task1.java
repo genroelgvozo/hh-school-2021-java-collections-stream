2e4c3cc8067ad33726cbf79384fb6717f3cb0484
Oleg Negrozov <o.negrozov@hh.ru>
2022-01-10 16:17:35 +0300

    solution

--- src/tasks/Task1.java
+++ src/tasks/Task1.java
@@ -3,10 +3,10 @@ package tasks;
 import common.Person;
 import common.PersonService;
 import common.Task;
-
-import java.util.Collections;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /*
@@ -18,10 +18,30 @@ import java.util.stream.Collectors;
  */
 public class Task1 implements Task {
 
-  // !!! Редактируйте этот метод !!!
+  /*
+   Такой кейс в реальности возникает достаточно часто
+   Например, нам надо с фильтрами и пагинацией (а для пагинации какая-то фиксированная сортировка обятельна) вытащить из базы тех же персон
+   Но данные нужны из некоторых таблиц, которые не требуются в фильтрации,
+   а некоторые вообще OneToMany и множат персон при джойне, а значит не удастся сделать нормальную пагинацию с limit+offset
+   Поэтому обычно пагинацией получают айдишки (применяя сортировку и фильтры)
+   а после по айдишкам вытаскивают все данные, приджойнивая уже необходимое. И вот это нужно отсортировать в порядке айдишек
+
+   Асимптотика - O(n)
+   */
   private List<Person> findOrderedPersons(List<Integer> personIds) {
-    Set<Person> persons = PersonService.findPersons(personIds);
-    return Collections.emptyList();
+    Map<Integer, Person> idPersonMap = PersonService.findPersons(personIds).stream() // обычно называю просто personMap, так как ключ Integer подразумевает  что там id, но можно и так
+      .collect(Collectors.toMap(Person::getId, Function.identity()));
+    return personIds.stream()
+        .map(idPersonMap::get)
+        .filter(Objects::nonNull) // Этот фильтр большинство пропускали, и на самом деле это нормально. Но иногда он необходим
+        // Естественно если айдишки переданны извне, то по ним может что-то не найтись,
+        // но в этом случае обычно не требуется сортировать, тут скорее кейс, что описан выше
+        // И тогда вопрос, а как это - мы селектнули из базы айди, а потом селектим персон и не находим
+        // Ну во-первых в ридонли без транзакций повторный селект может и не найти что нужно
+        // Но еще более частый кейс - айдишки получили из другого источника. Например база elasticsearch, которая хорошо подходит для полнотекстового поиска.
+        // И оттуда обычно получают айди найденных документов,
+        // а сами документы уже ищут в базе, и из-за лага индексации (которого не избежать), может быть разница. В таком случае filter необходим
+        .collect(Collectors.toList());
   }
 
   @Override
